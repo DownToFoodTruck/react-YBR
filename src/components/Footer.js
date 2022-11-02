491672fd3fd80856f2789f57741919e1d517daf1
KillytheBid <70246094+KillytheBid@users.noreply.github.com>
2022-11-01 21:08:49 -0500

    Remote Images
    switched images to github paths

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,44 +1,40 @@
 function Footer() {
-    return (
+  return (
+    <>
+      <h2 id="scrolling-text" className="mobile" data-text="Yellow Brick Road">
+        Yellow Brick Road
+      </h2>
+      <h2
+        id="scrolling-text"
+        className="desktop"
+        data-text="A Yellow Brick Road Collaboration"
+      >
+        A Yellow Brick Road Collaboration
+      </h2>
 
-    <><h2
-            id="scrolling-text"
-            className="mobile"
-            data-text="Yellow Brick Road"
-        >
-            Yellow Brick Road
-        </h2>
-            <h2
-            id="scrolling-text"
-            className="desktop"
-            data-text="A Yellow Brick Road Collaboration"
-            >
-                A Yellow Brick Road Collaboration
-            </h2>
-            
-            <div className="footer-top-right">
+      <div className="footer-top-right">
+        <div className="about-us-button">
+          <button className="about-us" id="open-modal">
+            ABOUT YBR
+          </button>
+        </div>
 
-                <div className="about-us-button">
-                    <button className="about-us" id="open-modal">
-                        ABOUT YBR
-                    </button>
-                </div>
-                
-                <div id="overlay">
-                    <div id="modal">
-                        <div id="modal-button-right">
-                            <button id="close-modal">Close</button>
-                        </div>
-                        <img
-                            src="/home/will/Development/Website/YBR/React/react-YBR/src/Images/YBR-Group.png"
-                            alt="YBR Group"
-                            className="ybr-image" />
-                        <br />
-                    </div>
-                </div>
-            </div></>
-
-)
+        <div id="overlay">
+          <div id="modal">
+            <div id="modal-button-right">
+              <button id="close-modal">Close</button>
+            </div>
+            <img
+              src="https://github.com/DownToFoodTruck/react-YBR/blob/7c94bd4a752c603ba394b17f32a0a46be30a86c2/src/Images/YBR-Group.png"
+              alt="YBR Group"
+              className="ybr-image"
+            />
+            <br />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,41 @@
 function Header() {
-    return (
+  return (
     <header>
-        <div className="header-contact-button">
-          <a href="mailto:hey@downtofoodtruck.com" id="contact">
-            <button className="contact-button">
-              <img
-                src="/home/will/Development/Website/YBR/React/react-YBR/src/Home//home/will/Development/Website/YBR/React/react-YBR/src/Images/email-icon.png"
-                alt="Contact"
-              />
-            </button>
-          </a>
-        </div>
-        <div className="header-follow-button">
-          <a
-            href="https://www.instagram.com/downtofoodtruck/"
-            id="follow"
-            target="_blank"
-          >
-            <button className="contact-button">
-              <img
-                src="/home/will/Development/Website/YBR/React/react-YBR/src/Images/instagram-icon.png"
-                alt="Instagram"
-              />
-            </button>
-          </a>
-        </div>
-        <div className="top-right desktop">
-          <button className="guest" onclick="window.location.href='index.html'">
-            Continue As Guest
+      <div className="header-contact-button">
+        <a href="mailto:hey@downtofoodtruck.com" id="contact">
+          <button className="contact-button">
+            <img
+              src="https://github.com/DownToFoodTruck/react-YBR/blob/7c94bd4a752c603ba394b17f32a0a46be30a86c2/src/Images/email-icon.png"
+              alt="Contact"
+            />
           </button>
-        </div>
-        <img
-          src="/home/will/Development/Website/YBR/React/react-YBR/src/Images/Logo-Blue-2500x1500.png"
-          className="over-img"
-        />
-      </header>
-)
+        </a>
+      </div>
+      <div className="header-follow-button">
+        <a
+          href="https://www.instagram.com/downtofoodtruck/"
+          id="follow"
+          target="_blank"
+        >
+          <button className="contact-button">
+            <img
+              src="https://github.com/DownToFoodTruck/react-YBR/blob/7c94bd4a752c603ba394b17f32a0a46be30a86c2/src/Images/instagram-icon.png"
+              alt="Instagram"
+            />
+          </button>
+        </a>
+      </div>
+      <div className="top-right desktop">
+        <button className="guest" onclick="window.location.href='index.html'">
+          Continue As Guest
+        </button>
+      </div>
+      <img
+        src="https://github.com/DownToFoodTruck/react-YBR/blob/7c94bd4a752c603ba394b17f32a0a46be30a86c2/src/Images/Logo-Blue-2500x1500.png"
+        className="over-img"
+      />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
