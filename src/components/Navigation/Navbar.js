714a43829efe1a3b1a858979cf14b1416fe1e3e6
KillytheBid <70246094+KillytheBid@users.noreply.github.com>
2022-11-18 17:43:04 -0600

    relative filepaths
    changed filepaths to be relative

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,71 +1,65 @@
-import React from 'react'
-import {FaBars} from 'react-icons/fa'
-import { Nav,
-	NavbarContainer,
-	NavLogo,
-	MobileIcon,
-	NavMenu,
-	NavItem,
-	NavLinks,
-	NavBtn,
-	NavBtnLink
-} from './NavbarElements'
-import DTFT from '/Users/krshinn/Documents/GitHub/react-YBR/src/Images/Logo-Blue-2500x1500.png';
-import Email from '/Users/krshinn/Documents/GitHub/react-YBR/src/Images/email-icon.png'
-import Insta from '/Users/krshinn/Documents/GitHub/react-YBR/src/Images/instagram-icon.png'
-
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
+import DTFT from "../../Images/Logo-Blue-2500x1500.png";
+import Email from "../../Images/email-icon.png";
+import Insta from "../../Images/instagram-icon.png";
 
 const Navbar = ({ toggle }) => {
   return (
-	<>
-		<Nav>
-			<NavbarContainer>
-				<NavLogo to="/">
-					<img src={DTFT} alt='Logo'/>
-				</NavLogo>
-				<MobileIcon onClick={toggle}>
-					<FaBars />
-				</MobileIcon>
-				<NavMenu>
-					<NavItem>
-						<NavLinks to=''>
-							Link
-						</NavLinks>
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">
+            <img src={DTFT} alt="Logo" />
+          </NavLogo>
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="">Link</NavLinks>
+            </NavItem>
 
-					</NavItem>
+            <NavItem>
+              <NavLinks to="/">Link</NavLinks>
+            </NavItem>
 
-					<NavItem>
-						<NavLinks to='/'>
-							Link
-						</NavLinks>
+            <NavItem>
+              <NavLinks
+                to={{ pathname: "https://www.instagram.com/downtofoodtruck" }}
+                target="_blank"
+              >
+                <img className="nav-img" src={Insta} alt="Instagram" />
+              </NavLinks>
+            </NavItem>
 
-					</NavItem>
+            <NavItem>
+              <NavLinks
+                to={{ pathname: "mailto:hey@downtofoodtruck.com" }}
+                target="_blank"
+              >
+                <img className="nav-img" src={Email} alt="Email" />
+              </NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/Login">Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
 
-					<NavItem>
-						<NavLinks to={{pathname: "https://www.instagram.com/downtofoodtruck"}} target="_blank">
-							<img className='nav-img' src={Insta} alt='Instagram'/>
-						</NavLinks>
-
-					</NavItem>
-					
-					<NavItem>
-						<NavLinks to={{pathname: 'mailto:hey@downtofoodtruck.com'}} target="_blank">
-							<img className='nav-img' src={Email} alt='Email'/>
-						</NavLinks>
-					
-					</NavItem>
-
-				</NavMenu>
-				<NavBtn>
-					<NavBtnLink to="/Login">
-						Sign In
-					</NavBtnLink>
-				</NavBtn>
-
-			</NavbarContainer>
-		</Nav>
-	</>
-  )
-}
-
-export default Navbar
+export default Navbar;
