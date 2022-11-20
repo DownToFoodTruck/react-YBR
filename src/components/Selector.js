bd19c453ba7b59828fbfb694a5bef88709ffa228
KillytheBid <70246094+KillytheBid@users.noreply.github.com>
2022-11-20 13:30:15 -0600

    Dropdown pop
    changed value usestate from string to array
    
    mapped value in the return to be multiple dropdowns

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,41 +1,39 @@
-import React, {useState} from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';  
-import {Container ,Dropdown,  DropdownButton} from 'react-bootstrap'; 
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Dropdown, DropdownButton } from "react-bootstrap";
 
 export default function Selector() {
+  const [value, setValue] = useState([]);
+  const [error, setError] = useState("");
 
-  const [value, setValue] = useState(""); 
-  const [error, setError] = useState("")
+  //POPULATE TAG DROPDOWN
+  //Make set, append i for i in truck categories, jam into tag drop
+  async function fetchSelectionList() {
+    try {
+      const url = "/apiTAG";
+      const rawRes = await fetch(url);
+      const rawResJSON = await rawRes.json();
 
-    //POPULATE TAG DROPDOWN
-    //Make set, append i for i in truck categories, jam into tag drop
-    async function fetchSelectionList() {
+      setValue(rawResJSON);
+      console.log(rawResJSON);
+    } catch (err) {
+      console.log(err);
+      setError("Nothing Retrieved");
+    }
+  }
+  fetchSelectionList();
 
-          try{
-          const url = "/apiTAG"
-          const rawRes = await fetch(url)
-          const rawResJSON = await rawRes.json()
-
-          setValue(rawResJSON)
-          console.log(rawResJSON)
-          }
-          catch(err) {
-            console.log(err)
-            setError("Nothing Retrieved")
-          }
-          
-      }
-  fetchSelectionList()
-
-  return(
-        <div className="selector"> 
-
-                <Container className='selector-container'>  
-                <DropdownButton id="truck-selector" title="Cuisine">  
-                    <Dropdown.Item name="selector-value" value={value}>{value}{error}</Dropdown.Item>   
-                </DropdownButton>  
-                </Container>  
-
-        </div>
-  )
-};
+  return (
+    <div className="selector">
+      <Container className="selector-container">
+        <DropdownButton id="truck-selector" title="Cuisine">
+          {value.map((e) => (
+            <Dropdown.Item name="selector-value" value={e}>
+              {e}
+            </Dropdown.Item>
+          ))}
+        </DropdownButton>
+      </Container>
+    </div>
+  );
+}
