9f39cc67e90eca761c900ae74bd611bf12e3b7c9
KillytheBid <70246094+KillytheBid@users.noreply.github.com>
2022-11-21 21:15:44 -0600

    queries

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,23 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';  
-import NavToggle from '../components/Navigation/NavToggle.js';
-import Selector from '../components/Selector.js';
-import Footer from '../components/Footer.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavToggle from "../components/Navigation/NavToggle.js";
+import Selector from "../components/Selector.js";
+import Footer from "../components/Footer.js";
 
 export default function Home() {
-
   return (
     <>
-      
       <NavToggle />
 
       {/* <Header /> */}
 
-      <div className='home-body'>
+      <div className="home-body">
+        <Selector />
 
-      <Selector />
-
-      <div className='about-section'></div>
-
+        <div className="about-section"></div>
       </div>
 
       <Footer />
--- src/components/TruckDisplay.js
+++ src/components/TruckDisplay.js
@@ -1,15 +1,25 @@
+import * as ReactDOM from "react-dom";
+
 export default function tagSelected(param) {
   async function executeQuery() {
     try {
       const url = "/api?tag=" + param;
       const rawRes = await fetch(url);
       let product = await rawRes.json();
-
-      console.log(product);
-      // console.log(rawResJSON);
+      // console.log(product);
+      return product;
     } catch (err) {
       console.log(err);
     }
   }
-  executeQuery();
+
+  async function returnQuery() {
+    const queryResult = await executeQuery();
+    console.log(queryResult);
+    // console.log(queryToHTML);
+    // ReactDOM.render(queryToHTML, document.getElementById("root"));
+  }
+  returnQuery();
+  // const queryToHTML = returnQuery();
+  // return queryToHTML.map((e) => <div>{e.Name}</div>);
 }
