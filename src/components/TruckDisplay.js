55bd2a778346ecb1516b04db9c6ff544932b16f4
KillytheBid <70246094+KillytheBid@users.noreply.github.com>
2022-12-04 12:19:48 -0600

    rendering
    just worried about getting things onto the screen

--- src/components/TruckDisplay.js
+++ src/components/TruckDisplay.js
@@ -3,15 +3,13 @@ import TruckSelector from "./TruckSelector";
 
 export const TruckDisplay = ({ name, img }) => {
   console.log({ name }, { img });
-  // console.log("NAME TYPE  ", Object.entries({ name }).values);
   const namer =
     { name } == null
       ? ""
       : Object.entries({ name }).map(
           ([key, value]) =>
-            `<div>${value.map((e) => Object.entries(e).key)}</div>`
+            `<div>${value.map((e) => Object.entries(e)[0])}</div>`
         );
-  // const nameStuff = namer.map((e) => <div>{e}</div>);
   return (
     <div className="truck-display">
       {namer}
