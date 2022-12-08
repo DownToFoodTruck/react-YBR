06d030124c66e9619804c8cbbdd4eee6cc7aa528
KillytheBid <70246094+KillytheBid@users.noreply.github.com>
2022-12-07 18:23:07 -0600

    conditional rendering
    if the object item is "NULL" return empty string

--- src/components/IndividualModal.js
+++ src/components/IndividualModal.js
@@ -7,7 +7,7 @@ const IndividualModal = (props) => {
 
   const truckInfo = props.truckData;
   console.log(truckInfo);
-  
+
   return (
     <>
       <div className="truck-modal" onClick={props.onClose}>
@@ -22,22 +22,55 @@ const IndividualModal = (props) => {
           <div className="truck-data">
             <h2>{truckInfo.Name}</h2>
             {/* <div>{truckInfo.PermitName}</div> */}
-            <p><h3>{truckInfo.Address}</h3></p>
-            <p>{truckInfo.Phone}</p>
-            <a style={{ textDecoration: 'none' }} href={truckInfo.Site} target="_blank">{truckInfo.Site}</a>
-            <p>{truckInfo.Email}</p>
-            <p>{truckInfo.Description}</p>
-            <p>{truckInfo.Hours_of_Operation}</p>
-            <div>Cuisine tags: {truckInfo.Tags}</div> 
+            <p>
+              <h3>{truckInfo.Address != "NULL" ? truckInfo.Address : ""}</h3>
+            </p>
+            <p>{truckInfo.Phone != "NULL" ? truckInfo.Phone : ""}</p>
+            <a
+              style={{ textDecoration: "none" }}
+              href={truckInfo.Site != "NULL" ? truckInfo.Site : ""}
+              target="_blank"
+            >
+              {truckInfo.Site != "NULL" ? truckInfo.Site : ""}
+            </a>
+            <p>{truckInfo.Email != "NULL" ? truckInfo.Email : ""}</p>
+            <p>
+              {truckInfo.Description != "NULL"
+                ? truckInfo.Description.toUpperCase()
+                : ""}
+            </p>
+            <p>
+              {truckInfo.Hours_of_Operation != "NULL"
+                ? truckInfo.Hours_of_Operation
+                : ""}
+            </p>
+            <div>Cuisine tags: {truckInfo.Tags}</div>
             {/* <div>{truckInfo.Profile}</div> */}
 
             <div className="truck-modal-pics">
-              <img src={truckInfo.P1} />
-              <img src={truckInfo.P2} />
-              <img src={truckInfo.P3} />
+              <img
+                src={
+                  truckInfo.P1 != "NULL"
+                    ? truckInfo.P1
+                    : "../Images/Truck-Avatar.png"
+                }
+              />
+              <img
+                src={
+                  truckInfo.P2 != "NULL"
+                    ? truckInfo.P2
+                    : "../Images/Truck-Avatar.png"
+                }
+              />
+              <img
+                src={
+                  truckInfo.P3 != "NULL"
+                    ? truckInfo.P2
+                    : "../Images/Truck-Avatar.png"
+                }
+              />
             </div>
           </div>
-
         </div>
       </div>
     </>
