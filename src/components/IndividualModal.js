62932bafe2e7f9115091d0ab6f14fdf28fc6bd8c
Karl <99865019+krshinn@users.noreply.github.com>
2023-01-02 15:45:23 -0600

    Modal and css adjustment
    Ind Modal adjustment to P3 and tweaks to CSS

--- src/components/IndividualModal.js
+++ src/components/IndividualModal.js
@@ -11,10 +11,9 @@ const IndividualModal = (props) => {
   return (
     <>
       <div className="truck-modal" onClick={props.onClose}>
-        <div
-          className="truck-modal-container"
-          onClick={(event) => event.stopPropagation()}
-        >
+
+        <div className="truck-modal-container" onClick={(event) => event.stopPropagation()}>
+          
           <button className="about-modal-close-btn" onClick={props.onClose}>
             X
           </button>
@@ -22,21 +21,21 @@ const IndividualModal = (props) => {
           <div className="truck-data">
             <h2>{truckInfo.Name}</h2>
             {/* <div>{truckInfo.PermitName}</div> */}
+
             <p>
               <h3>
-                <a
-                  href={
-                    truckInfo.Address != "NULL"
-                      ? "https://www.google.com/maps/place/" +
-                        truckInfo.Address.replace(" ", "+")
-                      : "#"
-                  }
+                <a 
+                  href={truckInfo.Address != "NULL" ? 
+                  "https://www.google.com/maps/place/" + truckInfo.Address.replace(" ", "+") 
+                  : "#"}
                 >
-                  HERE
+                  {truckInfo.Address != "NULL" ? truckInfo.Address : ""}
                 </a>
               </h3>
             </p>
+
             <p>{truckInfo.Phone != "NULL" ? truckInfo.Phone : ""}</p>
+
             <a
               style={{ textDecoration: "none" }}
               href={truckInfo.Site != "NULL" ? truckInfo.Site : ""}
@@ -44,46 +43,32 @@ const IndividualModal = (props) => {
             >
               {truckInfo.Site != "NULL" ? truckInfo.Site : ""}
             </a>
+
             <p>{truckInfo.Email != "NULL" ? truckInfo.Email : ""}</p>
             <p>
               {truckInfo.Description != "NULL"
                 ? truckInfo.Description.toUpperCase()
                 : ""}
             </p>
+
             <p>
-              {truckInfo.Hours_of_Operation != "NULL"
-                ? truckInfo.Hours_of_Operation
-                : ""}
+              {truckInfo.Hours_of_Operation != "NULL" ? truckInfo.Hours_of_Operation : ""}
             </p>
             <div>Cuisine tags: {truckInfo.Tags}</div>
             {/* <div>{truckInfo.Profile}</div> */}
 
             <div className="truck-modal-pics">
-              <img
-                src={
-                  truckInfo.P1 != "NULL"
-                    ? truckInfo.P1
-                    : "../Images/Truck-Avatar.png"
-                }
-              />
-              <img
-                src={
-                  truckInfo.P2 != "NULL"
-                    ? truckInfo.P2
-                    : "../Images/Truck-Avatar.png"
-                }
-              />
-              <img
-                src={
-                  truckInfo.P3 != "NULL"
-                    ? truckInfo.P2
-                    : "../Images/Truck-Avatar.png"
-                }
-              />
+
+              <img src={truckInfo.P1 != "NULL" ? truckInfo.P1 : "../Images/Truck-Avatar.png"} />
+              <img src={truckInfo.P2 != "NULL" ? truckInfo.P2 : "../Images/Truck-Avatar.png"} />
+              <img src={truckInfo.P3 != "NULL" ? truckInfo.P3 : "../Images/Truck-Avatar.png"} />
+
             </div>
+
             <div onClick={() => alert("MAYBE A SICK ASS PING OF THE VENDOR")}>
               WHERE AM I???
             </div>
+
           </div>
         </div>
       </div>
