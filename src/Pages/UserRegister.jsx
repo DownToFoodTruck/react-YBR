31f451ec680ba094fbfe5982d60b027a6663384e
Karl <99865019+krshinn@users.noreply.github.com>
2023-02-15 20:52:01 -0600

    update

--- src/Pages/UserRegister.jsx
+++ src/Pages/UserRegister.jsx
@@ -39,26 +39,25 @@ export default function UserRegister() {
 
   useEffect(() => {
     const result = EMAIL_REGEX.test(email);
-    console.log(result);
-    console.log(email);
+    // console.log(result);
+    // console.log(email);
     setValidEmail(result);
   }, [email]);
 
   useEffect(() => {
     const result = USER_REGEX.test(user);
-    console.log(result);
-    console.log(user);
+    // console.log(result);
+    // console.log(user);
     setValidName(result);
   }, [user]);
 
   useEffect(() => {
     const result = PWD_REGEX.test(pwd);
-    console.log(result);
-    console.log(pwd);
+    // console.log(result);
+    // console.log(pwd);
     setValidPwd(result);
     const match = pwd === matchPwd;
     setValidMatch(match);
-    console.log()
   }, [pwd, matchPwd]);
 
   useEffect(() => {
